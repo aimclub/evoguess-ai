b2604b7aca08f0884d05a425f9123eded22e404e
alpavlenko <art-pavlenko@mail.ru>
2023-01-16 17:37:04 +0300

    added support for python3.7

--- instance/module/variables/impl/backdoor.py
+++ instance/module/variables/impl/backdoor.py
@@ -1,6 +1,15 @@
 from ..variables import *
 
-from math import prod
+try:  # for python3.8 and greater
+    from math import prod
+except ImportError:  # for python3.7
+    from operator import mul
+    from functools import reduce
+
+
+    def prod(_list):
+        return reduce(mul, _list, 1)
+
 from copy import copy
 from itertools import compress
 from util.iterable import concat, list_of, slice_by, to_oct, to_bin
