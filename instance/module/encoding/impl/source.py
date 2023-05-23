f0cb463e7cde856c771a303e43bfbe6f92aed46b
alpavlenko <art-pavlenko@mail.ru>
2023-05-23 16:58:51 +0300

    try to fix header in source encoding

--- instance/module/encoding/impl/source.py
+++ instance/module/encoding/impl/source.py
@@ -7,18 +7,29 @@ from typings.searchable import Supplements
 
 class SourceData(EncodingData):
     def __init__(self, lines: str = None):
-        self._lines = lines
+        line, tail = lines.split('\n', maxsplit=1)
+        if line.startswith('p'):
+            parts = line.split()
+            self._lines = tail
+            self.max_lit = int(parts[2])
+            self.clauses = int(parts[3])
+        else:
+            self._lines = lines
+            self.max_lit = 0
+            self.clauses = 0
 
     def source(self, supplements: Supplements = ((), ())) -> str:
         assumptions, constraints = supplements
+        clauses = self.clauses + len(constraints) + len(assumptions)
+        header = f'p cnf {self.max_lit} {clauses}\n' if self.clauses else ''
         return ''.join([
-            self._lines, *(f'{x} 0\n' for x in assumptions),
+            header, self._lines, *(f'{x} 0\n' for x in assumptions),
             *(' '.join(map(str, c)) + ' 0\n' for c in constraints),
         ])
 
     @property
     def max_literal(self) -> int:
-        return 0
+        return self.max_lit
 
 
 class Source(Encoding):
